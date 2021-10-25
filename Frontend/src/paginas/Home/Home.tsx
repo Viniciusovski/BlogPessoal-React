9d291d6686cf38463edfbf31019e4e9f4eabe0a2
unknown <vini98.2012@gmail.com>
2021-10-25 16:18:32 -0300

    Aula 01

---
+++ Frontend/src/paginas/Home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+    return (
+        <>
+            <h1 className = "titulo">Home</h1>
+            <img src="https://i.imgur.com/gzdy2Lob.jpg" alt="Cachorro" className= "img"/>
+        </>
+    )
+}
+
+export default Home;
