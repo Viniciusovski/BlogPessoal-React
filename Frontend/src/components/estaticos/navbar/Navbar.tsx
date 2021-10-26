28396f02652d1ad6f9eb57ec00ab543589295804
unknown <vini98.2012@gmail.com>
2021-10-26 15:53:10 -0300

    Aula 02 - colocando grid em navbar

--- Frontend/src/components/estaticos/navbar/Navbar.tsx
+++ Frontend/src/components/estaticos/navbar/Navbar.tsx
@@ -1,49 +1,60 @@
-import React from 'react';
-import { AppBar, Toolbar, Typography, Box } from '@material-ui/core';
-import './Navbar.css'
+import React from "react";
+import { AppBar, Toolbar, Typography, Box, Grid } from "@material-ui/core";
+import "./Navbar.css";
 function Navbar() {
-    return (
-        <>
-            <AppBar position="static">
-                <Toolbar variant="dense" className="toolbar"> 
-                    <Box style={{ cursor: "pointer" }} >
-                        <Typography variant="h5" color="inherit">
-                            BlogPessoal
-                        </Typography>
-                    </Box>
+  return (
+    <>
+      <AppBar position="static">
+        <Toolbar variant="dense" className="toolbar">
+          <Box style={{ cursor: "pointer" }}>
+            <Typography variant="h5" color="inherit">
+              BlogPessoal
+            </Typography>
+          </Box>
 
-                    <Box display="flex" justifyContent="start" className = "menu">
-                        <Box mx={1} style={{ cursor: "pointer" }}>
-                            <Typography variant="h6" color="inherit">
-                                home
-                            </Typography>
-                        </Box>
-                        <Box mx={1} style={{ cursor: "pointer" }}>
-                            <Typography variant="h6" color="inherit">
-                                postagens
-                            </Typography>
-                        </Box>
-                        <Box mx={1} style={{ cursor: "pointer" }}>
-                            <Typography variant="h6" color="inherit">
-                                temas
-                            </Typography>
-                        </Box>
-                        <Box mx={1} style={{ cursor: "pointer" }}>
-                            <Typography variant="h6" color="inherit">
-                                cadastrar tema
-                            </Typography>
-                        </Box>
-                        <Box mx={1} style={{ cursor: "pointer" }}>
-                            <Typography variant="h6" color="inherit">
-                                logout
-                            </Typography>
-                        </Box>
-                    </Box>
+          <Grid container spacing={2} justifyContent="space-between">
+            <Grid item xs={6}>
+              <Box style={{ cursor: "pointer" }}>
+                <Typography variant="h5" color="inherit">
+                  BlogPessoal
+                </Typography>
+              </Box>
+            </Grid>
 
-                </Toolbar>
-            </AppBar>
-        </>
-    )
+            <Grid item xs={6}>
+              <Box display="flex" justifyContent="start" className="menu">
+                <Box mx={1} style={{ cursor: "pointer" }}>
+                  <Typography variant="h6" color="inherit">
+                    home
+                  </Typography>
+                </Box>
+                <Box mx={1} style={{ cursor: "pointer" }}>
+                  <Typography variant="h6" color="inherit">
+                    postagens
+                  </Typography>
+                </Box>
+                <Box mx={1} style={{ cursor: "pointer" }}>
+                  <Typography variant="h6" color="inherit">
+                    temas
+                  </Typography>
+                </Box>
+                <Box mx={1} style={{ cursor: "pointer" }}>
+                  <Typography variant="h6" color="inherit">
+                    cadastrar tema
+                  </Typography>
+                </Box>
+                <Box mx={1} style={{ cursor: "pointer" }}>
+                  <Typography variant="h6" color="inherit">
+                    logout
+                  </Typography>
+                </Box>
+              </Box>
+            </Grid>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
 }
 
 export default Navbar;
