d0850fe2222150f119fa9c533fe6fbc07b827fea
unknown <vini98.2012@gmail.com>
2021-10-27 20:57:47 -0300

    adicionando um menu em home

--- Frontend/src/components/estaticos/navbar/Navbar.tsx
+++ Frontend/src/components/estaticos/navbar/Navbar.tsx
@@ -1,56 +1,47 @@
 import React from "react";
-import { AppBar, Toolbar, Typography, Box, Grid } from "@material-ui/core";
+import { AppBar, Toolbar, Typography, Box } from "@material-ui/core";
 import "./Navbar.css";
+import MenuComponent from "../Menu/MenuComponent";
 function Navbar() {
   return (
     <>
       <AppBar position="static">
         <Toolbar variant="dense" className="toolbar">
-          <Box style={{ cursor: "pointer" }}>
-            <Typography variant="h5" color="inherit">
-              BlogPessoal
-            </Typography>
-          </Box>
 
-          <Grid container spacing={2} justifyContent="space-between">
-            <Grid item xs={6}>
-              <Box style={{ cursor: "pointer" }}>
-                <Typography variant="h5" color="inherit">
-                  BlogPessoal
+          <Box className="boxTopo" display="flex">
+          
+            <Box style={{ cursor: "pointer" }}>
+              <Typography variant="h5" color="inherit">
+                BlogPessoal
+              </Typography>
+            </Box>
+
+            <Box display="flex" justifyContent="flex-end">
+              <Box mx={1} style={{ cursor: "pointer", flexDirection: "row" }}>
+                <Typography variant="h6" color="inherit">
+                  HOME
                 </Typography>
               </Box>
-            </Grid>
-
-            <Grid item xs={6}>
-              <Box display="flex" justifyContent="start" className="menu">
-                <Box mx={1} style={{ cursor: "pointer" }}>
-                  <Typography variant="h6" color="inherit">
-                    home
-                  </Typography>
-                </Box>
-                <Box mx={1} style={{ cursor: "pointer" }}>
-                  <Typography variant="h6" color="inherit">
-                    postagens
-                  </Typography>
-                </Box>
-                <Box mx={1} style={{ cursor: "pointer" }}>
-                  <Typography variant="h6" color="inherit">
-                    temas
-                  </Typography>
-                </Box>
-                <Box mx={1} style={{ cursor: "pointer" }}>
-                  <Typography variant="h6" color="inherit">
-                    cadastrar tema
-                  </Typography>
-                </Box>
-                <Box mx={1} style={{ cursor: "pointer" }}>
-                  <Typography variant="h6" color="inherit">
-                    logout
-                  </Typography>
-                </Box>
+              <Box mx={1} style={{ cursor: "pointer" }}>
+                <Typography variant="h6" color="inherit">
+                  POSTAGENS
+                </Typography>
               </Box>
-            </Grid>
-          </Grid>
+              <Box mx={1} style={{ cursor: "pointer" }}>
+                <Typography variant="h6" color="inherit">
+                  TEMAS
+                </Typography>
+              </Box>
+              <Box mx={1} style={{ cursor: "pointer" }}>
+                <Typography variant="h6" color="inherit">
+                  CADASTRAR TEMA
+                </Typography>
+              </Box>
+              <Box mx={1} style={{ cursor: "pointer" }}>
+                <MenuComponent />
+              </Box>
+            </Box>
+            </Box>        
         </Toolbar>
       </AppBar>
     </>
