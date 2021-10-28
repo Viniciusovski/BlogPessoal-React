d90b092c8d2b3e3b0b2b8c08d2725e6c2127b07d
unknown <vini98.2012@gmail.com>
2021-10-28 12:09:27 -0300

    Implementando as Rotas de Home e Login e organizando a estilização

--- Frontend/src/components/estaticos/navbar/Navbar.tsx
+++ Frontend/src/components/estaticos/navbar/Navbar.tsx
@@ -2,46 +2,54 @@ import React from "react";
 import { AppBar, Toolbar, Typography, Box } from "@material-ui/core";
 import "./Navbar.css";
 import MenuComponent from "../Menu/MenuComponent";
+import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <>
       <AppBar position="static">
         <Toolbar variant="dense" className="toolbar">
-
           <Box className="boxTopo" display="flex">
-          
-            <Box style={{ cursor: "pointer" }}>
+            <Box className='cursor'>
               <Typography variant="h5" color="inherit">
                 BlogPessoal
               </Typography>
             </Box>
 
-            <Box display="flex" justifyContent="flex-end">
-              <Box mx={1} style={{ cursor: "pointer", flexDirection: "row" }}>
+            <Box display="flex" justifyContent="start">
+              <Box mx={1} className='cursor'>
                 <Typography variant="h6" color="inherit">
                   HOME
                 </Typography>
               </Box>
-              <Box mx={1} style={{ cursor: "pointer" }}>
+              <Box mx={1} className='cursor'>
                 <Typography variant="h6" color="inherit">
                   POSTAGENS
                 </Typography>
               </Box>
-              <Box mx={1} style={{ cursor: "pointer" }}>
+              <Box mx={1} className='cursor'>
                 <Typography variant="h6" color="inherit">
                   TEMAS
                 </Typography>
               </Box>
-              <Box mx={1} style={{ cursor: "pointer" }}>
+              <Box mx={1} className='cursor'>
                 <Typography variant="h6" color="inherit">
                   CADASTRAR TEMA
                 </Typography>
               </Box>
-              <Box mx={1} style={{ cursor: "pointer" }}>
+
+              <Link to='/login' className='text-decorator-none'>
+                <Box mx={1} className='cursor'>
+                  <Typography variant="h6" color="inherit">
+                    LOGOUT
+                  </Typography>
+                </Box>
+              </Link>
+
+              <Box mx={1} className='cursor'>
                 <MenuComponent />
               </Box>
             </Box>
-            </Box>        
+          </Box>
         </Toolbar>
       </AppBar>
     </>
