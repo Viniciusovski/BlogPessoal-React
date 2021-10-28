1fdb78d0246adf949d3ef341f06a571c89ebf1ea
unknown <vini98.2012@gmail.com>
2021-10-28 20:23:39 -0300

    alterando combinação de cores das páginas

--- Frontend/src/components/estaticos/navbar/Navbar.tsx
+++ Frontend/src/components/estaticos/navbar/Navbar.tsx
@@ -10,36 +10,36 @@ function Navbar() {
         <Toolbar variant="dense" className="toolbar">
           <Box className="boxTopo" display="flex">
             <Box className='cursor'>
-              <Typography variant="h5" color="inherit">
+              <Typography variant="h5" className="cor-font-typography">
                 BlogPessoal
               </Typography>
             </Box>
 
             <Box display="flex" justifyContent="start">
               <Box mx={1} className='cursor'>
-                <Typography variant="h6" color="inherit">
+                <Typography variant="h6" className="cor-font-typography">
                   HOME
                 </Typography>
               </Box>
               <Box mx={1} className='cursor'>
-                <Typography variant="h6" color="inherit">
+                <Typography variant="h6" className="cor-font-typography">
                   POSTAGENS
                 </Typography>
               </Box>
               <Box mx={1} className='cursor'>
-                <Typography variant="h6" color="inherit">
+                <Typography variant="h6" className="cor-font-typography">
                   TEMAS
                 </Typography>
               </Box>
               <Box mx={1} className='cursor'>
-                <Typography variant="h6" color="inherit">
+                <Typography variant="h6" className="cor-font-typography">
                   CADASTRAR TEMA
                 </Typography>
               </Box>
 
               <Link to='/login' className='text-decorator-none'>
                 <Box mx={1} className='cursor'>
-                  <Typography variant="h6" color="inherit">
+                  <Typography variant="h6" className="cor-font-typography">
                     LOGOUT
                   </Typography>
                 </Box>
