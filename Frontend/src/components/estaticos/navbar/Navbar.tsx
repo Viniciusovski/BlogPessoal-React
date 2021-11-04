329cdf9bdae29fe76b110c1b27ad67d7d54b1a7f
unknown <vini98.2012@gmail.com>
2021-11-04 17:12:26 -0300

    criação e conexação com backend do tema e postagem

--- Frontend/src/components/estaticos/navbar/Navbar.tsx
+++ Frontend/src/components/estaticos/navbar/Navbar.tsx
@@ -2,13 +2,29 @@ import React from "react";
 import { AppBar, Toolbar, Typography, Box, Grid } from "@material-ui/core";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
+import {useHistory} from 'react-router-dom';
+import useLocalStorage from "react-use-localstorage";
+
 function Navbar() {
+  const [token, setToken] = useLocalStorage('token');
+  let history = useHistory();
+
+  function goLogout(){
+    setToken('')
+    alert('Usuário deslogado')
+    history.push('login')
+  }
   return (
     <>
       <AppBar position="static">
         <Toolbar variant="dense" className="toolbar">
           <Box className="boxTopo" display="flex">
-            <Grid container spacing={35} justifyContent="space-between" alignItems="center">
+            <Grid
+              container
+              spacing={35}
+              justifyContent="space-between"
+              alignItems="center"
+            >
               <Grid item xs={6}>
                 <Box className="cursor">
                   <Typography variant="h5" className="cor-font-typography">
@@ -24,34 +40,49 @@ function Navbar() {
                   alignItems="center"
                   className="margin-left"
                 >
+                  <Link to="/home" className="text-decorator-none">
                   <Box mx={1} className="cursor hover">
                     <Typography variant="h6" className="cor-font-typography">
                       HOME
                     </Typography>
                   </Box>
+                  </Link>
+
+                  <Link to="/posts" className="text-decorator-none">
                   <Box mx={1} className="cursor hover">
                     <Typography variant="h6" className="cor-font-typography">
                       POSTAGENS
                     </Typography>
                   </Box>
-                  <Box mx={1} className="hover">
+                  </Link>
+
+                  <Link to="/temas" className="text-decorator-none">
+                  <Box mx={1} className="cursor hover">
                     <Typography variant="h6" className="cor-font-typography">
                       TEMAS
                     </Typography>
                   </Box>
-                  <Box mx={1} className="hover">
-                    <Typography variant="h6" className="cor-font-typography" noWrap>
+                  </Link>
+
+                  <Link to="/formularioTema" className="text-decorator-none">
+                  <Box mx={1} className="cursor hover">
+                    <Typography
+                      variant="h6"
+                      className="cor-font-typography"
+                      noWrap
+                    >
                       CADASTRAR TEMA
                     </Typography>
                   </Box>
-
-                  <Box mx={1} className="hover">
-                    <Link to="/login" className="text-decorator-none">
+                  </Link>
+                  
+                    <Box mx={1} className="hover" onClick={goLogout}>
                       <Typography variant="h6" className="cor-font-typography">
                         LOGOUT
                       </Typography>
-                    </Link>
-                  </Box>
+                    </Box>
+                  
+
                 </Box>
               </Grid>
             </Grid>
--- Frontend/src/paginas/cadastroUsuario/CadastroUsuario.tsx
+++ Frontend/src/paginas/cadastroUsuario/CadastroUsuario.tsx
@@ -2,7 +2,7 @@ import React, {useState, useEffect, ChangeEvent } from "react";
 import { useHistory } from "react-router";
 import User from "../../models/User";
 import { cadastroUsuario } from "../../services/Service";
-import { Button, Grid, TextField, Typography } from "@material-ui/core";
+import { Button, Grid, TextField, Typography } from "@mui/material";
 import { Box } from "@material-ui/system";
 //import { Link } from "react-router-dom";
 import "./CadastroUsuario.css";
